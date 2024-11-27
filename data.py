4e886ff4f7e92a9bab5bd2beddb62e4403a6e26f
Patrick Alexandre <palexandre822@gmail.com>
2024-11-27 11:49:27 -0700

    added quiz dataset in python

---
+++ data.py
@@ -0,0 +1,35 @@
+quizzes = [
+  {
+    "id": 1,
+    "question": "What is Naruto Uzumaki's favorite food?",
+    "answers": [
+      {"id": 1, "isCorrect": False, "text": "Soba"},
+      {"id": 2, "isCorrect": False, "text": "Onigiri"},
+      {"id": 3, "isCorrect": True, "text": "Ramen"},
+      {"id": 4, "isCorrect": False, "text": "Dango"}
+    ],
+    "points": 3
+  },
+  {
+    "id": 2,
+    "question": "Which village does Ay come from?",
+    "answers": [
+      {"id": 1, "isCorrect": False, "text": "Sunagakure"},
+      {"id": 2, "isCorrect": True, "text": "Kumogakure"},
+      {"id": 3, "isCorrect": False, "text": "Kirigakure"},
+      {"id": 4, "isCorrect": False, "text": "Iwagakure"}
+    ],
+    "points": 5
+  },
+  {
+    "id": 3,
+    "question": "What is the name of the third Hokage's summon?",
+    "answers": [
+      {"id": 1, "isCorrect": True, "text": "Enma"},
+      {"id": 2, "isCorrect": False, "text": "Son Goku"},
+      {"id": 3, "isCorrect": False, "text": "Aoda"},
+      {"id": 4, "isCorrect": False, "text": "Fukasaku"}
+    ],
+    "points": 10
+  }
+]
